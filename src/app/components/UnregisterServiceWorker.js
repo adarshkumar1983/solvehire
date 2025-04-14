970ee3d783547cf2e6f5187dfac611a136897718
adarshkumar1983 <adarshrajput1914@gmail.com>
2025-04-14 16:24:50 +0530

    Delete cache storage

--- src/app/components/UnregisterServiceWorker.js
+++ src/app/components/UnregisterServiceWorker.js
@@ -4,16 +4,30 @@ import { useEffect } from 'react';
 
 export default function UnregisterServiceWorker() {
   useEffect(() => {
+    // 🔹 Unregister service workers
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.getRegistrations()
-        .then(function(registrations) {
+        .then((registrations) => {
           for (let registration of registrations) {
             registration.unregister();
           }
         })
-        .catch(function(err) {
-          console.log('Service Worker unregister failed: ', err);
+        .catch((err) => {
+          console.log('Service Worker unregister failed:', err);
         });
+    }
+
+    // 🔹 Delete all old caches
+    if ('caches' in window) {
+      caches.keys().then((cacheNames) => {
+        cacheNames.forEach((cacheName) => {
+          caches.delete(cacheName).then((success) => {
+            if (success) {
+              console.log(`Cache ${cacheName} deleted.`);
+            }
+          });
+        });
+      });
     }
   }, []);
 
